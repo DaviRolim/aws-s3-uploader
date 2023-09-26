94e9e9edc32c2e7bc9c457727e4ab8b431808930
DaviRolim <davirolim94@gmail.com>
2023-09-26 11:29:39 -0300

    sending files to s3

--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require github.com/gofiber/fiber/v2 v2.49.2
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/aws/aws-sdk-go v1.45.16 // indirect
 	github.com/google/uuid v1.3.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"mime/multipart"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 )
 
+func uploadImageToS3(bucketName string, fileHeader *multipart.FileHeader, cfg *aws.Config) error {
+	// Open the image file
+	file, err := fileHeader.Open()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new session with default session credentials
+	// The session the S3 Uploader will use
+	sess := session.Must(session.NewSession(cfg))
+
+	// S3 service client the Upload manager will use.
+	svc := s3.New(sess)
+
+	// Configure the S3 object input parameters
+	input := &s3.PutObjectInput{
+		Body:          file,
+		Bucket:        aws.String(bucketName),
+		Key:           aws.String(fileHeader.Filename),
+		ContentType:   aws.String(fileHeader.Header["Content-Type"][0]),
+		ContentLength: aws.Int64(fileHeader.Size),
+	}
+
+	fmt.Println("Created input")
+
+	// Upload the image to S3
+	_, err = svc.PutObject(input)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Uploaded image to s3")
+
+	return nil
+}
+
 func main() {
+	// Load env vars
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	awsKey := os.Getenv("AWS_KEY")
+	awsSecret := os.Getenv("AWS_SECRET")
+	// Initialize AWS Credentials
+	credentials := credentials.NewStaticCredentials(awsKey, awsSecret, "")
+
+	cfg := aws.NewConfig().WithRegion("us-west-1").WithCredentials(credentials)
+
 	// Fiber instance
 	app := fiber.New()
 
@@ -22,6 +79,37 @@ func main() {
 
 	// Routes
 	app.Get("/", hello)
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		// Parse the multipart form:
+		form, err := c.MultipartForm()
+		if err != nil {
+			return err
+		}
+		// => *multipart.Form
+
+		// Get all files from "documents" key:
+		files := form.File["images"]
+		// => []*multipart.FileHeader
+		fmt.Println("Got files")
+		// Loop through files:
+		for _, file := range files {
+			fmt.Println(file.Filename, file.Size, file.Header["Content-Type"][0])
+
+			// Save the files to disk:
+			// err := c.SaveFile(file, fmt.Sprintf("./%s", file.Filename))
+			// Check for errors
+			// if err != nil {
+			// 	return err
+			// }
+			// Send file to s3
+
+			fmt.Println("Started go routine")
+			go uploadImageToS3("voodireito", file, cfg)
+
+		}
+		return nil
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
